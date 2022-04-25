3c0bcbe46d2754d26fa062c9f815be4692015360
DameonWebb <dameonlwebb@gmail.com>
2022-04-25 16:15:57 -0700

    added a response to the server connection

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// write your code here
+// loads the http module
+const http = require('http');
+// create a web server that can send headers and a body response
+const server = http.createServer((req, res) => {
+    if(req.url === '/') {
+        res.writeHead(200,{ 
+        'content-type': 'text/plain',
+         })
+        res.end('Hello World');
+    }
+});
+//Add a listener so that your server will have a way to handle incoming connections
+server.listen(8080, 'localhost', null, () => console.log('Listening on port 8080...')) 
+//When the server starts: log a message so you will know where it is listening and the port number (3030, 3333, 8080, or 8000 are common)
+
+
